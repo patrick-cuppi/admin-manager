c77997b0ba829dfcd71d7447dfd1b4f76436a141
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-10-09 13:34:09 -0300

    feat: sequelize config

---
+++ src/database/index.js
@@ -0,0 +1,27 @@
+import { Sequelize } from "sequelize";
+import config from "../config/database";
+// import Model from "../models/xxx";
+
+const models = [];
+
+class Database {
+  constructor() {
+    this.connection = new Sequelize(config);
+    this.init();
+    this.associate();
+  };
+
+  init() {
+    models.forEach((model) => model.init(this.connection));
+  };
+
+  associate() {
+    models.forEach((model) => {
+      if(model.associate) {
+        model.associate(this.connection.models)
+      };
+    });
+  };
+};
+
+export default new Database();
