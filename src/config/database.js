9f645770c8187f3b053db0ed5d168f0e961845cd
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-10-10 13:13:20 -0300

    feat: table users migrate

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
+import pg from "pg-hstore";
 require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
+  dialectModule: pg,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import AdminJS from "adminjs";
 import AdminJSExpress from "@adminjs/express";
 import AdminJSSequelize from "@adminjs/sequelize";
 import express from "express";
+import User from "./models/user";
 
 AdminJS.registerAdapter(AdminJSSequelize);
 
@@ -12,7 +13,7 @@ const app = express();
 const adminJS = new AdminJS({
     databases: [],
     rootPath: '/admin',
-    resources: []
+    resources: [User]
 });
 
 const router = AdminJSExpress.buildRouter(adminJS);
@@ -21,3 +22,4 @@ app.use(adminJS.options.rootPath, router);
 app.listen(5000, () => {
     console.log('AdminJS is under http://localhost:5000/admin')
 })
+
